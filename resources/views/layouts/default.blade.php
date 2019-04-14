fa7d8acca51e33a02bb3187d271d5b6833ec4fce
mhk00123 <mhk00123@gmail.com>
2019-04-14 17:18:13 +0000

    init Default pages

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,9 +5,29 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    {{--Link CSS File --}}
+    <link rel="stylesheet" href="/css/app.css">
+
     <title>@yield('title', 'Weibo App') - Laravel</title>
 </head>
 <body>
-    @yield('content')
+
+    {{--Navbar--}}
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="/">Weibo App</a>
+            <ul class="navbar-nav justify-content-end">
+                <li class="nav-item"><a class="nav-link" href="/help">Help</a></li>
+                <li class="nav-item" ><a class="nav-link" href="#">Login</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    {{--Content--}}
+    <div class="container">
+        @yield('content')
+    </div>
+
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,6 +1,19 @@
 @extends('layouts.default')
 
 @section('content')
-    <h1>Main Pages</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            你現在所看到的是
+            <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入門</a>
+            的入門示例項目主頁
+        </p>
+
+        <p>一切，將從這里開始</p>
+
+        <p>
+            <a href="#" class="btn btn-lg btn-success" role="button">Sign Up Now!</a>
+        </p>
+    </div>
 
     @stop
