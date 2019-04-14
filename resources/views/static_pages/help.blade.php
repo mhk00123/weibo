1ad25654082c30a687985466b272d71e347e0c68
mhk00123 <mhk00123@gmail.com>
2019-04-14 16:41:57 +0000

    init 3 Static Page

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+@section('title', 'Help')
+@section('content')
+    <h1>Help Pages</h1>
+
+@stop
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,6 +15,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
