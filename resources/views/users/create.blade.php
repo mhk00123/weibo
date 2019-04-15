9ba9c94437d66c6698b1215906cf22cec056df33
mhk00123 <mhk00123@gmail.com>
2019-04-15 13:05:50 +0000

    Add Sign Up Page

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,7 +1,37 @@
 @extends('layouts.default')
-
 @section('title', 'Sign Up')
-@section('content')
-    <h1>Sign Up Pages</h1>
 
-    @stop
+@section('content')
+    <div class="offset-md-2 col-md-8">
+        <div class="card ">
+            <div class="card-header">
+                <h5>Sign Up</h5>
+            </div>
+            <div class="card-body">
+                <form method="POST" action="{{ route('users.store') }}">
+                    <div class="form-group">
+                        <label for="name">Name : </label>
+                        <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">E-Mail : </label>
+                        <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">Password : </label>
+                        <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password_confirmation">Confirm Password : </label>
+                        <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Sign Up</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@stop
