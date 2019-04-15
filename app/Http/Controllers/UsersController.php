bee361f52aafc2b923655e680efed6988e078d78
mhk00123 <mhk00123@gmail.com>
2019-04-15 13:39:11 +0000

    增加heroku 使用 PostgreSQL的判斷

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,11 +20,23 @@ class UsersController extends Controller
 
     public function store(Request $request)
     {
+        //---(1)驗證
         //validate -> Laravel自帶用於驗證的function
         $this->validate($request, [
             'name' => 'required|max:50',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
+
+        //---(2)建立數據庫資料
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        //---(3)成功後引導到個人資料頁面
+        session()->flash('success', 'Welcome for your new journey');
+        return redirect()->route('users.show', [$user]);
     }
 }
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -19,6 +19,8 @@
     {{--Content--}}
     <div class="container">
         <div class="offset-md-1 col-md-10">
+            {{--Error Message--}}
+            @include('shared._messages')
 
             {{--Content--}}
             @yield('content')
